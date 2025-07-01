799cc12fcbe316a2826c27ff09dfae6fcf338c09
harsh <hs5686584@gmail.com>
2025-07-01 17:13:07 +0530

    get api && get data by id && post api for tour creation

--- natours-api/app.js
+++ natours-api/app.js
@@ -1,8 +1,54 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
+const { status } = require("wd/lib/commands");
 
-app.get("/", (req, resp) => {
-  resp.send("service started");
+app.use(express.json());
+
+const Tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get("/api/v1/tours", (req, resp) => {
+  resp.status(200).json({
+    status: "success",
+    results: Tours.length,
+    data: {
+      tour: Tours,
+    },
+  });
+});
+
+app.get("/api/v1/tours/:id", (req, resp) => {
+  const reqID = req.params.id * 1;
+  const searchedTour = Tours[reqID];
+
+  resp.status(200).json({
+    status: "success",
+    data: {
+      searchedTour,
+    },
+  });
+});
+
+app.post("/api/v1/tours", (req, resp) => {
+  const newId = Tours.length;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  Tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(Tours),
+    (err) => {
+      resp.status(201).json({
+        status: "success",
+        data: {
+          newTour,
+        },
+      });
+    }
+  );
 });
 
 app.listen(8011, () => {
