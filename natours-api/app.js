a4d926e026f0206f26b68cddcb664bcc6604d970
Harsh <hs5686584@gmail.com>
2025-07-11 00:44:58 +0530

    updated code

--- natours-api/app.js
+++ natours-api/app.js
@@ -1,4 +1,3 @@
-// 1.) Imports
 const express = require("express");
 const app = express();
 const fs = require("fs");
@@ -7,30 +6,16 @@ const morgan = require("morgan");
 const userRouter = require("./routes/userRoutes");
 const tourRouter = require("./routes/tourRoutes");
 
-// 2.) Middlewares
-
 app.use(morgan("dev"));
 
 app.use(express.json());
-app.use((req, resp, next) => {
-  console.log("hello from the middleware");
-  next();
-});
 
 app.use((req, resp, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
-// 3.) Controllers
-
-// ---> User controller
-
-// ---> User Routes
-
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
-
-// 5.) Server starts
 
 module.exports = app;
